4674f9be48a154dbb6421c0e4806e4b2552436cb
Aldar <ochirov-3@mail.ru>
2024-06-10 21:37:30 +0300

    - installed library for work with excel - basic auth

---
+++ app/Http/Middleware/authBasic.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class authBasic
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::onceBasic()) {
+            return response()->json(['message' => 'auth failed'], Response::HTTP_UNAUTHORIZED);
+        } else {
+            return $next($request);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Middleware\authBasic;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware([authBasic::class]);
